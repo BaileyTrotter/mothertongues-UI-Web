724640a4aaf40033c4ff3da3609a40290ae06f40
BaileyTrotter <96272684+BaileyTrotter@users.noreply.github.com>
2024-01-19 15:52:19 -0800

    Update dict_cached.js

--- projects/mtd/src/assets/js/dict_cached.js
+++ projects/mtd/src/assets/js/dict_cached.js
@@ -14,7 +14,7 @@ var dataDict = [
     "definition": "duck (any) ", 
     "word": "chat", 
     audio: [
-      { filename: 'BaileyTrotter/mothertongues-UI-web/projects/mtd/src/assests/duck.wav' }
+      { filename: '..duck.wav' }
     ],
     "theme": "birds", 
     "secondary_theme": NaN, 
