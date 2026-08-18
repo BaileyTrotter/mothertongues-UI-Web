45b080efa063978fe8c8272313fd53fa1474cb8d
BaileyTrotter <96272684+BaileyTrotter@users.noreply.github.com>
2026-08-18 23:50:14 +0000

    Deploying to gh-pages from @ BaileyTrotter/mothertongues-UI-Web@f43bbd202e1dd89f36c4de4dfd6c77cd530a247d 🚀

--- assets/js/dict_cached.js
+++ assets/js/dict_cached.js
@@ -1,9 +1,16 @@
 var dataDict = [
     {
-        source: "noun dictionary", 
-        theme: "mammals", 
-        definition: "caribou", 
-        word: "udzi", 
-        audio: [{filename: "00520_caribou_ttm_Johnson-Edwards_PC_08-09-2023_Selkirk-Noun-Dictionary.wav"}]
+        "source": "noun dictionary", 
+        "theme": "mammals", 
+        "definition": "caribou", 
+        "word": "udzi", 
+        "audio": [{filename: "00520_caribou_ttm_Johnson-Edwards_PC_08-09-2023_Selkirk-Noun-Dictionary.wav"}]
+    },
+    {
+        "source": "noun dictionary", 
+        "theme": "mammals", 
+        "definition": "caribouuu", 
+        "word": "udziiii", 
+        "audio": [{filename: ""}]
     }
 ]
