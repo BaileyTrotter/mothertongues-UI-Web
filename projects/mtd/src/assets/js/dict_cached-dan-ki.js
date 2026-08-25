583e3b5a7be2e5f1d7d24de0b66f54c59ad15464
BaileyTrotter <96272684+BaileyTrotter@users.noreply.github.com>
2026-08-25 16:31:53 -0700

    Update dict_cached-dan-ki.js

--- projects/mtd/src/assets/js/dict_cached-dan-ki.js
+++ projects/mtd/src/assets/js/dict_cached-dan-ki.js
@@ -1,10 +1,10 @@
 var dataDict = [
   {
-    source: 'Tommy McGinty and Johnson Edwards (Selkirk Indian Language Noun Dictionary 1977)',
-    theme: 'mammals',
-    definition: 'fox',
-    word: 'nuntthi',
-    audio: [
-      {filename: '00523_fox_ttm_Johnson-Edwards_PC_08-09-2023_Selkirk-Noun-Dictionary.wav'}
+    "source:" "Tommy McGinty and Johnson Edwards (Selkirk Indian Language Noun Dictionary 1977)",
+    "theme": "mammals",
+    "definition": "fox",
+    "word": "nuntthi",
+    "audio": [
+      {"filename": "00523_fox_ttm_Johnson-Edwards_PC_08-09-2023_Selkirk-Noun-Dictionary.wav"}
           ]
   }
