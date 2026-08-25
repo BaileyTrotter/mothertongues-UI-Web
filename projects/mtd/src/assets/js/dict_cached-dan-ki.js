fd78808d912a6116f2b6d2a3cfb34eff0afe633d
BaileyTrotter <96272684+BaileyTrotter@users.noreply.github.com>
2026-08-25 15:57:11 -0700

    Update dict_cached-dan-ki.js

--- projects/mtd/src/assets/js/dict_cached-dan-ki.js
+++ projects/mtd/src/assets/js/dict_cached-dan-ki.js
@@ -7,26 +7,7 @@ var dataDict = [
       { 'test information here' }
     ],
     word: 'nuntthi',
-    compare_form: "nunthi",
-    sort_form: "nuntthi",
-    sorting_form: [18, 6, 18, 42, 4],
-    entryID: '891',
     audio: [
-      { filename: '00523_fox_ttm_Johnson-Edwards_PC_08-09-2023_Selkirk-Noun-Dictionary.wav' }
-    ]
-  },
-  {
-    source: 'Tommy McGinty and Johnson Edwards (Selkirk Indian Language Noun Dictionary 1977)',
-    theme: 'mammals',
-    definition: 'black fox',
-    optional: [
-      { 'other test information here' }
-    ],
-    word: 'nuntthi dene\u0301t\u02bcro',
-    compare_form: 'nunthi denet\u02bcro',
-    sort_form: 'nuntthi dene\u0301t\u02bcro',
-    sorting_form: [18, 6, 18, 42, 4, 11, 3, 18, 3, 40, 50, 5],
-    entryID: '892',
-    audio: '',
+      {filename: '00523_fox_ttm_Johnson-Edwards_PC_08-09-2023_Selkirk-Noun-Dictionary.wav'}
+          ]
   }
-]
